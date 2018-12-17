03a292a29cf6d5e3f5cd4340d393636e325854e8
Tiwaa <ctbruks@gmail.com>
2018-12-16 22:10:52 -0500

    first commit

--- choropleth_dc.js
+++ choropleth_dc.js
@@ -17,10 +17,13 @@ var geojsonURL = "neighbourhoods_dc.geojson"
 // Fetch GeoJSON and data to join
 d3.json(geojsonURL, function(error, json){
     d3.csv(dataCSV, function(error, data){
-
-      for (var i = 0; i < data.length; i++){
+      
+      // var newArray = []
+      for (var i = 0; i < data.length; i++) {
         var dataNeighbourhood = data[i].neighbourhood;
-        var dataBookings = data[i].number_of_reviews;
+        var dataBookings = data[i]['normalized value (per 1000 reviews)'] 
+        // newArray.push(+dataBookings)
+        
 
         for (var j = 0; j < json.features.length; j ++) {
           var jsonNeighbourhood = json.features[j].properties.neighbourhood;
@@ -29,6 +32,9 @@ d3.json(geojsonURL, function(error, json){
             json.features[j].properties.value = +dataBookings;
             break;
       }}}
+
+      // console.log(ss.ckmeans(newArray, 8))
+
       //Highlight polygon when mouse hovers
       function highlightFeature(e) {
         var layer = e.target;
@@ -51,34 +57,57 @@ d3.json(geojsonURL, function(error, json){
           mouseover: highlightFeature,
           mouseout: resetHighlight
         });
-        layer.bindTooltip('<center><b>Neighbourhood:</b> ' + feature.properties.neighbourhood + '<hr/><b>Total Number of Bookings:</b> ' + feature.properties.value.toLocaleString() + '</center>');
+        layer.bindTooltip('<center><b>Neighbourhood:</b> ' + feature.properties.neighbourhood + '<hr/><b>Number Of Bookings (per 1,000):</b> ' + feature.properties.value.toLocaleString() + '</center>');
       }
- //Initiate choropleth plugin      
+ //Initiate choropleth plugin  
+//  '#edcedf',  '#c994c7',  
  var choropleth = L.choropleth(json, {
-    valueProperty: 'value',
-    colors: ['#f7f4f9','#edcedf','#d4b9da','#c994c7','#df65b0','#e7298a','#ce1256','#91003f'],
-    steps: 8,
-    mode: 'k',
-    style: {
-      color: '#fff',
-      opacity: 1,
-      dashArray:'1',
-      weight: .8,
-      fillOpacity: .7
-    },
-    onEachFeature: onEachFeature
+  valueProperty: 'value',
+  colors: ['#f7f4f9','#d4b9da','#df65b0','#e7298a','#ce1256','#91003f'],
+  steps: 6,
+  mode: 'k',
+  style: {
+    color: '#fff',
+    opacity: 1,
+    dashArray:'1',
+    weight: .8,
+    fillOpacity: .7
+  },
+  onEachFeature: onEachFeature
 
-  }).addTo(map);
+}).addTo(map);
+
+// var legend = L.control({position: 'bottomleft'});
+
+// legend.onAdd = function (map) {
+
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         limits = choropleth.options.limits;
+//         colors = choropleth.options.colors;
+//         labels = [];
+
+//     // loop through our choropleth intervals and generate a label with a colored square for each interval
+//     for (var i = 0; i < limits.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + colors[i] + 1 + '"></i> ' +
+//             limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+');
+//     }
+
+//     return div;
+// };
+
+// legend.addTo(map);
+
   // Setting up the legend
   var legend = L.control({ position: "bottomleft" });
   legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "info legend");
-    var limits = choropleth.options.limits;
-    var colors = choropleth.options.colors;
-    var labels = [];
+    var div = L.DomUtil.create("div", "info legend"),
+     limits = choropleth.options.limits,
+      colors = choropleth.options.colors,
+      labels = [];
 
     // Add min & max
-    var legendInfo = "<h1>Total Number of Bookings</h1>" +
+    var legendInfo = "<h1>Number of Trips <i><div>(per 1,000)</div></i></h1>" +
       "<div class=\"labels\">" +
         "<div class=\"min\">" + limits[0] + "</div>" +
         "<div class=\"max\">" + limits[limits.length - 1].toLocaleString() + "</div>" +
